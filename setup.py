64921bf04bd3f41cf512a1c399402e4b52363af2
Robin Engels <103217628+robin-engels@users.noreply.github.com>
2025-10-22 13:13:57 +0000

    version bug fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mocha",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         "click",
