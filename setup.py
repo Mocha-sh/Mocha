70eb058ec529615be33c4d4a4c63a4c106d970cb
Robin Engels <103217628+robin-engels@users.noreply.github.com>
2025-10-23 05:30:56 +0000

    Hot fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mocha",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=[
         "click",
